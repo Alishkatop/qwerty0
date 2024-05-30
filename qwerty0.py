0a005540cdd3d7d428f1f1fa4fd06d4544d49d63
unknown <qwery0621@gmail.com>
2024-05-30 21:07:16 +0500

    qwerty0

---
+++ qwerty0.py
@@ -0,0 +1,37 @@
+
+# a = int(input("Введите любое чило: "))
+# b = 0 
+# while b <= a:
+#     print(b)
+#     b += 1
+
+
+# a = str(input("Введите любое слово: "))
+# print(a[::-1])
+
+# a = int(input("Введите любое число от 1 до 10: "))
+# b = 0
+# while b < 11:
+#     c = a * b
+#     print(a,'*',b,'=',c)
+#     b += 1
+
+# for i in range (1,10,2):
+#     print(" " *((9 - i) // 2) + str('*') * i )
+
+# a = int(input("Введите первое число: "))
+# b = int(input("Введите второе число: "))
+# while a <= b:
+#     print(a/b)9
+
+# a = int(input("Number 1 : "))
+# b = int(input ("Number 2 : "))
+# f = 0
+# for x in range(a,b,2):
+#     f += 1
+# print("Нечетных : ",f)
+# f = 0
+# a += 1
+# for x in range(a,b,2):
+#     f += 1
+# print ("Четных : ",f)
